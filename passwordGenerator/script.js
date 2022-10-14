ec706ff78b4897d389a067819b3a0bde0f4b13bb
saptha-home <sapthagirirmsh8@gmail.com>
2022-10-14 23:28:19 +0530

    password generator using html,css and js

---
+++ passwordGenerator/script.js
@@ -0,0 +1,44 @@
+function generatePassword() {
+    let generatedPassword = generateRandomPassword();
+
+    let input = document.getElementById("password");
+    input.value = generatedPassword;
+
+    let hintText = document.getElementById("hint");
+
+    hintText.innerText = ""
+}
+
+function copyText() {
+    let copyText = document.getElementById("password");
+    let hintText = document.getElementById("hint");
+
+    hintText.innerText = "copied"
+    navigator.clipboard.writeText(copyText.value);
+}
+
+function generateRandomPassword() {
+    let upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let loweCases = 'abcdefghijklmnopqrstuvwxyz';
+    let specialCharacters = '!@#$%^&*';
+    let numbers = '1234567890';
+
+    let i = 0;
+    let password = '';
+
+    while (i < 2) {
+        let upperCase = upperCases.split('')[Math.floor(Math.random() * upperCases.length)];
+
+        let lowercase = loweCases.split('')[Math.floor(Math.random() * loweCases.length)];
+
+        let specialCharacter = specialCharacters.split('')[Math.floor(Math.random() * specialCharacters.length)];
+
+        let number = numbers.split('')[Math.floor(Math.random() * numbers.length)];
+
+        password += `${upperCase}${lowercase}${specialCharacter}${number}`;
+
+        i++;
+    }
+
+    return password;
+}
