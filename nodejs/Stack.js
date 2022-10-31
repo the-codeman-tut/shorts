0d63a261de7df5452316fb22d1690114bc1734d4
saptha-home <sapthagirirmsh8@gmail.com>
2022-10-31 08:16:15 +0530

    Stack using array

---
+++ nodejs/Stack.js
@@ -0,0 +1,15 @@
+// LIFO - Last In First Out.
+
+let stack = [];
+
+stack.push("user1");
+stack.push("user2");
+stack.push("user3");
+
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
