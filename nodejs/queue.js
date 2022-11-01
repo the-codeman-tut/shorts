3e2bd29cc2e47208b17d094f1b0156cf74e097df
saptha-home <sapthagirirmsh8@gmail.com>
2022-11-01 08:15:11 +0530

    queue in js

---
+++ nodejs/queue.js
@@ -0,0 +1,15 @@
+// FIFO - First In First Out.
+
+let queue = [];
+
+queue.push("user1");
+queue.push("user2");
+queue.push("user3");
+
+console.log(queue);
+console.log(queue.shift());
+console.log(queue);
+console.log(queue.shift());
+console.log(queue);
+console.log(queue.shift());
+console.log(queue);
